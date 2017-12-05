df42e9cb5d38715b2af833316ff3cbe86a627f88
Shane Austrie <yoyo.rom.dev@gmail.com>
2017-12-04 21:37:53 -0500

    Maps loads from database and add sighting adds to database

--- M4/app/src/main/java/com/smokegod/cs2340/m3/InputActivity.java
+++ M4/app/src/main/java/com/smokegod/cs2340/m3/InputActivity.java
@@ -3,18 +3,22 @@ package com.smokegod.cs2340.m3;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.List;
 
 /**
  * Created by Vraka on 10/17/2017.
@@ -24,39 +28,73 @@ public class InputActivity extends Activity {
 
     private FusedLocationProviderClient mFusedLocationClient;
     private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+    private Spinner locationTypeSpinner, boroughSpinner;
+    private EditText addressET, cityET, zipET;
 
     @Override
     public void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_input);
 
-        if (ContextCompat.checkSelfPermission(InputActivity.this,
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+        //Not fully sure why this is here -Shane
+//        if (ContextCompat.checkSelfPermission(InputActivity.this,
+//                android.Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//
+//            // Should we show an explanation?
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(InputActivity.this,
+//                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+//
+//                // Show an explanation to the user *asynchronously* -- don't block
+//                // this thread waiting for the user's response! After the user
+//                // sees the explanation, try again to request the permission.
+//
+//            } else {
+//
+//                // No explanation needed, we can request the permission.
+//
+//                ActivityCompat.requestPermissions(InputActivity.this,
+//                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+//                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+//
+//                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+//                // app-defined int constant. The callback method gets the
+//                // result of the request.
+//            }
+//        } else {
+//            checkLocation();
+//        }
 
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(InputActivity.this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+        addressET = (EditText) findViewById(R.id.addressET);
+        cityET = (EditText) findViewById(R.id.cityET);
+        zipET = (EditText) findViewById(R.id.zipET);
 
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
+        locationTypeSpinner = (Spinner) findViewById(R.id.locationTypeSpinner);
+        String[] locationTypeSpinnerItems = new String[]{"1-2 Family Dwelling",
+                "3+ Family Apt. Building",
+                "3+ Family Mixed Use Building",
+                "Commercial Building",
+                "Vacant Lot",
+                "Construction Site",
+                "Hospital",
+                "Catch Basin/Sewer"};
+        ArrayAdapter<String> locationTypeAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                locationTypeSpinnerItems);
+        locationTypeSpinner.setAdapter(locationTypeAdapter);
 
-            } else {
+        boroughSpinner = (Spinner) findViewById(R.id.boroughSpinner);
+        String[] boroughSpinnerItems = new String[]{"MANHATTAN",
+                "STATEN ISLAND",
+                "QUEENS",
+                "BROOKLYN",
+                "BRONX"};
+        ArrayAdapter<String> borughAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item,
+                boroughSpinnerItems);
+        boroughSpinner.setAdapter(borughAdapter);
 
-                // No explanation needed, we can request the permission.
 
-                ActivityCompat.requestPermissions(InputActivity.this,
-                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        } else {
-            checkLocation();
-        }
     }
     @Override
     public void onRequestPermissionsResult(int requestCode,
@@ -80,7 +118,7 @@ public class InputActivity extends Activity {
                 return;
             }
 
-            // other 'case' lines to check for other
+            // other "case' lines to check for other
             // permissions this app might request
         }
     }
@@ -104,5 +142,54 @@ public class InputActivity extends Activity {
     public void goToMap(View v) {
         Intent i = new Intent(InputActivity.this, MapsActivity.class);
         startActivity(i);
+    }
+
+    public void submit(View v) {
+        if (!addressET.getEditableText().toString().isEmpty() ||
+                !cityET.getEditableText().toString().isEmpty() ||
+                !zipET.getEditableText().toString().isEmpty()) {
+            String address = addressET.getEditableText().toString() + ", " +
+                    cityET.getEditableText().toString() + zipET.getEditableText().toString();
+            String[] latlong = getLatLong(address);
+            if (latlong == null) {
+                Toast t = Toast.makeText(this, "Cant find latitude or longitude using that address", Toast.LENGTH_LONG);
+                t.show();
+            }
+            RatSighting rat = new RatSighting("", "", locationTypeSpinner.getSelectedItem().toString(),
+                    zipET.getEditableText().toString(), addressET.getEditableText().toString(),
+                    cityET.getEditableText().toString(), boroughSpinner.getSelectedItem().toString(),
+                    latlong[0], latlong[1]);
+
+            HTTPPostReq.addSighting(rat);
+            finish();
+
+        } else {
+            Toast t = Toast.makeText(this, "None of the input should be empty", Toast.LENGTH_LONG);
+            t.show();
+        }
+    }
+
+    private String[] getLatLong(String strAddress) {
+        Geocoder coder = new Geocoder(this);
+        List<Address> address;
+        String[] result = new String[2];
+
+        try {
+            address = coder.getFromLocationName(strAddress,5);
+            if (address == null) {
+                return null;
+            }
+            Address location=address.get(0);
+            location.getLatitude();
+            location.getLongitude();
+
+            result[0] = String.valueOf((double) (location.getLatitude() * 1E6));
+            result[1] = String.valueOf((double) (location.getLongitude() * 1E6));
+
+            return result;
+        } catch (Exception e) {
+            Log.d("InputActivity", e.toString());
+        }
+        return null;
     }
 }
--- M4/app/src/main/java/com/smokegod/cs2340/m3/MarkerInfoActivity.java
+++ M4/app/src/main/java/com/smokegod/cs2340/m3/MarkerInfoActivity.java
@@ -1,13 +1,56 @@
 package com.smokegod.cs2340.m3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MarkerInfoActivity extends AppCompatActivity {
+    String key, date, address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getExtra(savedInstanceState);
         setContentView(R.layout.activity_marker_info);
+
+        TextView keyTV = (TextView) findViewById(R.id.markerinfo_keyTV);
+        TextView dateTV = (TextView) findViewById(R.id.markerinfo_dateTV);
+        TextView addressTV = (TextView) findViewById(R.id.markerinfo_addressTV);
+
+        addressTV.setText(address);
+        dateTV.setText(date);
+        keyTV.setText(key);
+    }
+
+    private void getExtra(Bundle savedInstanceState) {
+        String markerString;
+        if (savedInstanceState == null) {
+            markerString = getIntent().getStringExtra("MARKER_STRING");
+        } else {
+            markerString = savedInstanceState.getString("MARKER_STRING");
+        }
+        if (markerString != null || !markerString.isEmpty()) {
+            String[] arr = markerString.split("\\n");
+            for (int i = 0; i < arr.length; i++) {
+               String[] arr2 = arr[i].split(": ");
+                switch(i) {
+                    case (0):
+                        key = arr2[1];
+                        break;
+                    case (1):
+                        date = arr2[1];
+                        break;
+                    case (2):
+                        address = arr2[1];
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        if (date == null || key == null || address == null) {
+            finish();
+        }
+
     }
 }
